3def570008e3ccdf3822a50a2e41e243578fb014
Suejung Shin <suejungshin@Suejungs-MacBook-Pro.local>
2019-12-19 10:57:01 -0800

    Move description to README instead of server file

--- README.md
+++ README.md
@@ -35,18 +35,18 @@ PROPERTIES
   DELETE /api/costHomeOwnership/properties
   Delete a property
 
-RATES (LOAN OFFERINGS)
+RATES (LOAN OFFERINGS)  (update name of endpoints noun that is not case sensitve, put the details here, don't put it in the code)
   GET /api/costHomeOwnership/rates?id
   Get all loan options for a property listing based on the query params cost, zipCode, term, type, downPay, credit, origYear
   Returns an array of loan option objects that have rateId, zipCode, apr, term, loanType, costLow, costHigh, downPaymentMin, lenderId
 
   POST /api/costHomeOwnership/rates
   Add a new loan option to a given property that match the query params
-  Returns the new image entry object
+  Returns the new loan offering entry object
 
   PUT /api/costHomeOwnership/rates?id
   Update the specified loan option
-  Returns the updated image entry object
+  Returns the updated loan offering entry object
 
   DELETE /api/costHomeOwnership/rates
   Delete a specific rate loan offering
@@ -54,7 +54,7 @@ RATES (LOAN OFFERINGS)
 LENDERS
   GET /api/costHomeOwnership/lenders?id
   Get basic lender info based on the lender id
-  Returns an array of basic lender information lenderId, originationYear, lenderLogoUrl, lenderNmls
+  Returns an array of basic lender information objects with lenderId, originationYear, lenderLogoUrl, lenderNmls
 
   POST /api/costHomeOwnership/lenders
   Add a new lender to the listing
@@ -65,6 +65,8 @@ LENDERS
 
   DELETE /api/costHomeOwnership/lenders
   Delete a specific lender
+
+
 
 
 ## Requirements
@@ -89,3 +91,161 @@ npm install
 
 
 # More Information
+
+
+/* Client side individual pages get rendered at:
+     http:// [[SERVER NAME OR IP ADDRESS]] :3001/?id= [[ NUMBER BETWEEN 1 AND 100 ]]
+   For example:
+     http://localhost:3001/?id=2
+ */
+
+/*  *******************
+       PROPERTIES
+************************ */
+
+/* ********  GET REQUEST *********
+GET request is sent to address:
+    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/properties?id=${id}
+
+Query parameter: id number between 1 and 100
+
+For example:
+    http://localhost:3001/api/costHomeOwnership/properties?id=2
+
+The result returned looks like the below:
+    [
+      {"propertyId":2,
+      "zipCode":"74716",
+      "redfinCostEstimate":780000,
+      "insuranceRate":"0.130",
+      "propertyTaxRate":"0.900"}
+    ]
+*/
+
+
+/* ********  POST REQUEST *********
+POST request is sent to address:
+    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/properties
+
+For example:
+    http://localhost:3001/api/costHomeOwnership/properties
+
+Ensure data to be sent follows below format:
+      {"propertyId":2,
+      "zipCode":"74716",
+      "redfinCostEstimate":780000,
+      "insuranceRate":"0.130",
+      "propertyTaxRate":"0.900"}
+
+Result returned: success code and console logged sucess message
+
+*/
+
+/* ********  PUT (UPDATE) REQUEST *********
+PUT request is sent to address:
+    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/properties?id=${id}
+
+Query parameter: id number between 1 and 100
+
+For example:
+    http://localhost:3001/api/costHomeOwnership/properties?id=2
+
+Ensure data to be sent follows below format:
+      {"propertyId":2,
+      "zipCode":"74716",
+      "redfinCostEstimate":780000,
+      "insuranceRate":"0.130",
+      "propertyTaxRate":"0.900"}
+
+Result returned: success code and copy of new object
+
+*/
+
+/* ********  DELETE REQUEST *********
+DELETE request is sent to address:
+    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/properties
+
+For example:
+    http://localhost:3001/api/costHomeOwnership/properties
+
+Ensure data to be sent follows below format:
+      {"propertyId":2}
+
+Result returned: success code and console logged sucess message
+*/
+
+
+/*  *******************
+       RATES
+************************ */
+
+/* ********  GET REQUEST *********
+GET request is sent to address:
+    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/rates?id=${id}
+
+Query parameters include:
+  cost, zipCode, term, type, downPay, credit, origYear
+
+For example:
+    http://localhost:3001/api/costHomeOwnership/rates?cost=2290000&zipCode=80354&term=30&type=Fixed&downPay=20&credit=740&origYear=2019
+
+The result returned looks like the below, based on entries of
+ loan options that would be available to a given person based on their
+ inputted information for cost, zipCode, term, type, downPay, credit, origYear
+[
+  {"rateId":27,
+  "zipCode":"80354",
+  "apr":"4.617",
+  "term":30,
+  "loanType":"Fixed",
+  "costLow":400000,
+  "costHigh":2500000,
+  "downPaymentMin":"0.0",
+  "creditMin":680,
+  "lenderId":1,
+  "originationYear":2019,
+  "lenderLogoUrl":"https://hrsf-fec-cho-lenderlogos.s3-us-west-1.amazonaws.com/10271_logo.gif",
+  "lenderNmls":619100
+},
+  {"rateId":125,
+  "zipCode":"80354",
+  "apr":"4.744",
+  "term":30,
+  "loanType":"Fixed",
+  "costLow":790000,
+  "costHigh":2000000,
+  "downPaymentMin":"20.0",
+  "creditMin":740,
+  "lenderId":3,
+  "originationYear":2019,
+  "lenderLogoUrl":"https://hrsf-fec-cho-lenderlogos.s3-us-west-1.amazonaws.com/7834_logo.gif",
+  "lenderNmls":772116}
+]
+*/
+
+
+/* ********  POST REQUEST *********
+POST request is sent to address:
+    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/rates
+
+For example:
+    http://localhost:3001/api/costHomeOwnership/rates
+
+Post requests are submitted to add potential loan options for buyers.
+The data to be sent looks like the below:
+  {"zipCode":"80354",
+  "apr":"4.617",
+  "term":30,
+  "loanType":"Fixed",
+  "costLow":400000,
+  "costHigh":2500000,
+  "downPaymentMin":"0.0",
+  "creditMin":680,
+  "lenderName": "FirstMidwestBank"}
+
+The database would have to be queried to find the lender name listed
+  {"lenderId":1,
+  "originationYear":2019,
+  "lenderLogoUrl":"https://hrsf-fec-cho-lenderlogos.s3-us-west-1.amazonaws.com/10271_logo.gif",
+  "lenderNmls":619100}
+*/
--- server/app.js
+++ server/app.js
@@ -12,38 +12,9 @@ app.use(cors());
 app.use(compression());
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-/* Client side individual pages get rendered at:
-     http:// [[SERVER NAME OR IP ADDRESS]] :3001/?id= [[ NUMBER BETWEEN 1 AND 100 ]]
-   For example:
-     http://localhost:3001/?id=2
- */
-
-
-/*  *******************
-       PROPERTIES
-************************ */
-
-/* ********  GET REQUEST *********
-GET request is sent to address:
-    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/properties?id=${id}
-
-Query parameter: id number between 1 and 100
-
-For example:
-    http://localhost:3001/api/costHomeOwnership/properties?id=2
-
-The result returned looks like the below:
-    [
-      {"propertyId":2,
-      "zipCode":"74716",
-      "redfinCostEstimate":780000,
-      "insuranceRate":"0.130",
-      "propertyTaxRate":"0.900"}
-    ]
-*/
+// PROPERTIES
 
 app.get('/api/costHomeOwnership/properties', async (req, res) => {
   // TODO - check security implications
@@ -52,7 +23,6 @@ app.get('/api/costHomeOwnership/properties', async (req, res) => {
     res.status(400).end('missing query parameters');
     return;
   }
-
   try {
     const [properties] = await controller.getPropertyData(id);
     res.json(keysToCamel(properties));
@@ -61,123 +31,20 @@ app.get('/api/costHomeOwnership/properties', async (req, res) => {
   }
 });
 
-/* ********  POST REQUEST *********
-POST request is sent to address:
-    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/properties
-
-For example:
-    http://localhost:3001/api/costHomeOwnership/properties
-
-Ensure data to be sent follows below format:
-      {"propertyId":2,
-      "zipCode":"74716",
-      "redfinCostEstimate":780000,
-      "insuranceRate":"0.130",
-      "propertyTaxRate":"0.900"}
-
-Result returned: success code and console logged sucess message
-
-*/
-
 app.post('/api/costHomeOwnership/properties', async (req, res) => {
   // Write function to post req.body to the database based on the propertyId. If the propertyId is already taken, confirm if the user wants it to be overwritten
 });
 
-
-/* ********  PUT (UPDATE) REQUEST *********
-PUT request is sent to address:
-    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/properties?id=${id}
-
-Query parameter: id number between 1 and 100
-
-For example:
-    http://localhost:3001/api/costHomeOwnership/properties?id=2
-
-Ensure data to be sent follows below format:
-      {"propertyId":2,
-      "zipCode":"74716",
-      "redfinCostEstimate":780000,
-      "insuranceRate":"0.130",
-      "propertyTaxRate":"0.900"}
-
-Result returned: success code and copy of new object
-
-*/
-
 app.put('/api/costHomeOwnership/properties', async (req, res) => {
 // Write function to update req.body to the database based on the propertyId. If the propertyId is already taken, confirm if the user wants it to be created new
 });
-
-
-/* ********  DELETE REQUEST *********
-DELETE request is sent to address:
-    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/properties
-
-For example:
-    http://localhost:3001/api/costHomeOwnership/properties
-
-Ensure data to be sent follows below format:
-      {"propertyId":2}
-
-Result returned: success code and console logged sucess message
-*/
 
 app.delete('/api/costHomeOwnership/properties', async (req, res) => {
 // Write function to delete req.body to the database based on the propertyId. If the propertyId doesn't exist, confirm with user what they want to delete
 });
 
 
-/* ****************************
-******************************* */
-
-
-/*  *******************
-       RATES
-************************ */
-
-/* ********  GET REQUEST *********
-GET request is sent to address:
-    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/rates?id=${id}
-
-Query parameters include:
-  cost, zipCode, term, type, downPay, credit, origYear
-
-For example:
-    http://localhost:3001/api/costHomeOwnership/rates?cost=2290000&zipCode=80354&term=30&type=Fixed&downPay=20&credit=740&origYear=2019
-
-The result returned looks like the below, based on entries of
- loan options that would be available to a given person based on their
- inputted information for cost, zipCode, term, type, downPay, credit, origYear
-[
-  {"rateId":27,
-  "zipCode":"80354",
-  "apr":"4.617",
-  "term":30,
-  "loanType":"Fixed",
-  "costLow":400000,
-  "costHigh":2500000,
-  "downPaymentMin":"0.0",
-  "creditMin":680,
-  "lenderId":1,
-  "originationYear":2019,
-  "lenderLogoUrl":"https://hrsf-fec-cho-lenderlogos.s3-us-west-1.amazonaws.com/10271_logo.gif",
-  "lenderNmls":619100
-},
-  {"rateId":125,
-  "zipCode":"80354",
-  "apr":"4.744",
-  "term":30,
-  "loanType":"Fixed",
-  "costLow":790000,
-  "costHigh":2000000,
-  "downPaymentMin":"20.0",
-  "creditMin":740,
-  "lenderId":3,
-  "originationYear":2019,
-  "lenderLogoUrl":"https://hrsf-fec-cho-lenderlogos.s3-us-west-1.amazonaws.com/7834_logo.gif",
-  "lenderNmls":772116}
-]
-*/
+// RATES
 
 app.get('/api/costHomeOwnership/rates', async (req, res) => {
   // TODO - check security implications
@@ -189,11 +56,9 @@ app.get('/api/costHomeOwnership/rates', async (req, res) => {
       return;
     }
   }
-
   const {
     cost, zipCode, term, type, downPay, credit, origYear,
   } = queries;
-
   try {
     const [rates] = await controller.getRates(
       cost, zipCode, term, type, downPay, credit, origYear,
@@ -203,34 +68,5 @@ app.get('/api/costHomeOwnership/rates', async (req, res) => {
     res.status(500).end('server could not retrieve rates data');
   }
 });
-
-/* ********  POST REQUEST *********
-POST request is sent to address:
-    http:// [[SERVER NAME OR IP ADDRESS]] :3001/api/costHomeOwnership/rates
-
-For example:
-    http://localhost:3001/api/costHomeOwnership/rates
-
-Post requests are submitted to add potential loan options for buyers.
-The data to be sent looks like the below:
-  {"zipCode":"80354",
-  "apr":"4.617",
-  "term":30,
-  "loanType":"Fixed",
-  "costLow":400000,
-  "costHigh":2500000,
-  "downPaymentMin":"0.0",
-  "creditMin":680,
-  "lenderName": "FirstMidwestBank"}
-
-The database would have to be queried to find the lender name listed
-  {"lenderId":1,
-  "originationYear":2019,
-  "lenderLogoUrl":"https://hrsf-fec-cho-lenderlogos.s3-us-west-1.amazonaws.com/10271_logo.gif",
-  "lenderNmls":619100}
-*/
-
-
-
 
 module.exports = app;
