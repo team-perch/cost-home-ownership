9bfb3128b4706d4a1e7f2edc1fcd722deabda51a
Suejung Shin <suejungshin@Suejungs-MacBook-Pro.local>
2020-01-15 10:22:17 -0800

    Update database address

--- database/postgres/pg-index.js
+++ database/postgres/pg-index.js
@@ -46,7 +46,7 @@ const createDbConn = async (scopeAuth) => {
   let pool;
   try {
     pool = new pg.Pool({
-      host: '3.19.29.239',
+      host: '3.134.116.61',
       user: 'suejungshin',
       database,
       password: 'postgres',
